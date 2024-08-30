ca8ae3ed5ffc1ee836c8191a27b711e7b82e5088
MD. Nazmul Huda <nazmulfarhan@gmail.com>
2024-08-31 04:57:14 +0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-sudo docker image rm -f  keycloak-with-rabbitmp-keycloak-2
-sudo docker image rm -f  keycloak-with-rabbitmp-receiver-service 
+sudo docker image rm -f  keycloak-with-rabbitmp-keycloak-2 # this is for deleting old images if any changes have been made, disable this if there is no changes in this service.
+sudo docker image rm -f  keycloak-with-rabbitmp-receiver-service # this is for deleting old images if any changes have been made, disable this if there is no changes in this service.
 # mvn clean install
 sudo docker compose up
