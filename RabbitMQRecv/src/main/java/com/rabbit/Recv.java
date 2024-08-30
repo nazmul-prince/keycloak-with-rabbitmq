25dbd45f1bd569da26d9442c4d8cc9006bc16f41
nazmul-prince <nazmulfarhan@gmail.com>
2024-08-31 04:56:03 +0600

    Integrate rabbit mq within keycloak

---
+++ RabbitMQRecv/src/main/java/com/rabbit/Recv.java
@@ -0,0 +1,32 @@
+package com.rabbit;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+public class Recv
+{
+
+    private final static String QUEUE_NAME = "q.role.mapper";
+    public static void main( String[] args ) throws IOException, TimeoutException {
+        System.out.println( "Hello World!" );
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+    }
+}
